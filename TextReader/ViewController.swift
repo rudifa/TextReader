58dad27e9846e95bc1b004d84de00efb5ae81a17
rudifa <rudi.farkas@gmail.com>
2019-08-26 15:25:22 +0200

    Initial Commit

---
+++ TextReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextReader
+//
+//  Created by Rudolf Farkas on 26.08.19.
+//  Copyright © 2019 Rudolf Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
